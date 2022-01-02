711eac6b7b2f4faa911c137ecd5f1ffd4cc45daf
Toney Tate <tdatate@gmail.com>
2022-01-01 23:46:22 -0500

    Create Css file and default text for game

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ function randomNum() {
 }
 let hasBlackJack = false;
 let isAlive = true;
+let message = ""
 
 let firstCard = randomNum();
 let secondCard = randomNum();
 let sum = firstCard + secondCard + 4
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card?")
+    message = ("Do you want to draw a new card? ")
 } else if (sum === 21) {
-    console.log("Awesome! You've got Blackjack! ")
-    hasBlackJack = true;
+    message = ("Wohoo! You've got Blackjack! ")
+    hasBlackJack = true
 } else {
-    console.log("You're out of the game!")
-    isAlive = false;
+    message = ("You're out of the game! ")
+    isAlive = false
 }
+
+// 3. Log it out!
+console.log(message)
