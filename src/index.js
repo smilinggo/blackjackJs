4304c18c5aed481920175b7bc9b2e6b9d10e0519
Toney Tate <tdatate@gmail.com>
2022-01-02 00:01:02 -0500

    Update logic to show result of random cards on screen

--- src/index.js
+++ src/index.js
@@ -4,20 +4,29 @@ function randomNum() {
 let hasBlackJack = false;
 let isAlive = true;
 let message = ""
+let messageEl = document.getElementById("message-el");
+let cardsEl = document.getElementById("cards-el");
+let sumEl = document.getElementById("sum-el");
 
 let firstCard = randomNum();
 let secondCard = randomNum();
-let sum = firstCard + secondCard + 4
+let sum = firstCard + secondCard
 
-if (sum <= 20) {
-    message = ("Do you want to draw a new card? ")
-} else if (sum === 21) {
-    message = ("Wohoo! You've got Blackjack! ")
-    hasBlackJack = true
-} else {
-    message = ("You're out of the game! ")
-    isAlive = false
+
+function startGame() {
+    if (sum <= 20) {
+        message = ("Do you want to draw a new card? ")
+    } else if (sum === 21) {
+        message = ("Wohoo! You've got Blackjack! ")
+        hasBlackJack = true
+    } else {
+        message = ("You're out of the game! ")
+        isAlive = false
+    }
+
+    // 3. Log it out!
+    messageEl.textContent = message;
+    cardsEl.textContent += firstCard + " " + secondCard
+    sumEl.textContent += sum;
+   
 }
-
-// 3. Log it out!
-console.log(message)
